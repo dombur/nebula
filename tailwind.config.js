119f67c9a4db243694a6602cd73b5adc9edd88b1
Dominik B <domburkart@gmail.com>
2022-03-28 18:22:14 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'black': '#111111',
+      'grey': '#aaaaaa',
+      'turblue': '#0000ff'
+    },
+    fontFamily: {
+      'body': ['Montserrat']
+    },
+    extend: {},
+  },
+  plugins: [""],
+}
